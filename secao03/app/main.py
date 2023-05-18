4cbefe014a41103a413af8a89a1e325b337dc7ce
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-05-18 09:07:58 -0300

    added get methods courses

--- secao03/app/main.py
+++ secao03/app/main.py
@@ -15,4 +15,13 @@ cursos = {
     }
 }
 
+@app.get('/cursos')
+async def get_cursos():
+    return cursos
 
+
+@app.get('/cursos/{curso_id}')
+async def get_curso(curso_id: int):
+    curso = cursos[curso_id]
+    curso.update({ "id": curso_id })
+    return curso
