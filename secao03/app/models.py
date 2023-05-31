011abcd219a7c496e6b85b3d989b7c31507c5e2b
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-05-31 17:38:30 -0300

    added new validations to anothers fields

--- secao03/app/models.py
+++ secao03/app/models.py
@@ -8,10 +8,24 @@ class Curso(BaseModel):
     horas: int
 
     @validator('titulo')
-    def validar_titulo(cls, value):
+    def validar_titulo(cls, value: str):
         palavras = value.split(' ')
         if len(palavras) < 3:
-            raise ValueError('O título deve ter pelo menos 3 palavras')
+            raise ValueError('O título deve ter pelo menos 3 palavras.')
+        if value.islower():
+            raise ValueError("O título deve ser capitalizado.")
+        return value
+
+    @validator('aulas')
+    def valida_aulas(cls, value: int):
+        if value < 12:
+            raise ValueError('Não pode haver mais de 12 aulas por curso.')
+        return value
+
+    @validator('horas')
+    def valida_horas(cls, value: int):
+        if value < 3:
+            raise ValueError('Não pode haver curso com menos de três horas de duração.')
         return value
 
 cursos = [
