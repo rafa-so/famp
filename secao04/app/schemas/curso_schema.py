a5c05e0507d30a7418a84cbd00654d594c8fff17
Rafael <rafa.forsoft.25@gmail.com>
2023-07-04 17:19:27 -0300

    Created curso model schema

---
+++ secao04/app/schemas/curso_schema.py
@@ -0,0 +1,12 @@
+from typing from Optional
+
+from pydantic import BaseModel as SCBaseModel
+
+class CursoSchema(SCBaseModel):
+    id: Optional[int]
+    titulo: str
+    aulas: int
+    horas: int
+
+    class Config:
+        orm_mode = True
